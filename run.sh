f4a4b5f36dd4260d3cfb5e12227a9c2358ebdd53
张琪 <cloudage@gmail.com>
2016-07-07 14:18:54 +0800

    ...

--- run.sh
+++ run.sh
@@ -6,5 +6,8 @@ docker rm imif_to_remote
 docker kill imif_to_self
 docker rm imif_to_self
 
-docker run -d --name 'imif_to_remote' -p 8001:8001 telecom_im_toremote
-docker run -d --name 'imif_to_self' -p 80:80 telecom_im_toself
+mkdir -p /var/log/nginx_dual_delegate/toremote
+mkdir -p /var/log/nginx_dual_delegate/toself
+
+docker run -d --name 'imif_to_remote' -p 8001:8001 -v /var/log/nginx_dual_delegate/toremote:/var/log/nginx telecom_im_toremote
+docker run -d --name 'imif_to_self' -p 80:80 -v /var/log/nginx_dual_delegate/toself:/var/log/nginx telecom_im_toself
